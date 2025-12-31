7d8dfd8e64a3c8bf7cfeeac50748663e469053aa
dzlk <dlb.diesel@gmail.com>
2025-12-31 07:54:08 +0500

    Autoinstalling markdownlint. Setup tab width for go

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'go',
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = false
+  end,
+})
+
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
